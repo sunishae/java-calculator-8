40416dd2abae022d018eaf4809f7d8b11ce9c2f3
sunishae <sunishae44@gmail.com>
2025-10-19 03:46:26 +0900

    feat: 주요 계산 로직 구현

--- src/main/java/calculator/domain/StringAddCalculator.java
+++ src/main/java/calculator/domain/StringAddCalculator.java
@@ -8,6 +8,7 @@ public class StringAddCalculator {
         this.parser = new StringParser();
     }
 
+    // 문자열에서 숫자 리스트 파싱
     public int calculate(String text) {
         if (text.isEmpty()) {
             return 0;
@@ -18,7 +19,7 @@ public class StringAddCalculator {
         return sum(numberStrings);
     }
 
-    // 덧셈
+    // 유효성 검사 및 덧셈
     public int sum(String[] numberStrings) {
         int sum = 0;
         for (String numberString : numberStrings) {
